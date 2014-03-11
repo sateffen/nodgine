c0ea12fe026820f385a8d20d9611042ae1944e72
Steffen <sateffen@gmail.com>
2014-03-11 22:09:24 +0100

    Extended documentation for $APPLICATION for issue #6

--- Tests/Application.js
+++ Tests/Application.js
@@ -1,12 +1,28 @@
+/**
+ * Unittest for the application object
+ *
+ * This tests only the load function and it's mechanisms, cause the other functions are trivial and hopefuly not buggy or to change
+ */
 
+/**
+ * Reference to the path object
+ *
+ * @private
+ * @type {path}
+ **/
 var mPath = require('path');
 
+/*
+* This Test checks, whether the $APPLICATION.load() function works properly, in conjunction with the addLoadPath function
+*/
 module.exports.load = function(test) {
     'use strict';
-    var a   = require('../Controller/Application.js'),
+    var a = require('../Bootstrap.js').$APPLICATION,
         path = mPath.resolve('./Tests/ApplicationTest/');
     a.addLoadPath(path);
 
-    test.equal(a.load('TestObject'), require('./ApplicationTest/TestObject.js'));
+    // test whether the loaded object and the directly required object is the same
+    test.equal(a.load('TestObject'), require('./ApplicationTest/TestObject.js'), '$APPLICATION.load: ' +
+        'Required and loaded object should be the same');
     test.done();
 };
