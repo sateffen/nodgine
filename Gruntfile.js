0823239b96672faa6dc41e4a833a5a555e53ef17
Steffen <sateffen@gmail.com>
2014-03-15 15:45:47 +0100

    Added Grunt for a build process

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        copy: {
+            main: {
+                files: [
+                    {expand: true, src: ['Controller/*'], dest: 'dist/', filter: 'isFile'},
+                    {expand: true, src: ['package.json'], dest: 'dist/'},
+                    {expand: true, src: ['bootstrap.js'], dest: 'dist/'}
+                ]
+            }
+        },
+        clean: {
+            dist: ['dist/']
+        },
+        nodeunit: {
+            all: ['Tests/*.js']
+        },
+        yuidoc: {
+            compile: {
+                name: '<%= pkg.name %>',
+                description: '<%= pkg.description %>',
+                version: '<%= pkg.version %>',
+                options: {
+                    paths: '.',
+                    outdir: 'Docs/'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-yuidoc');
+
+    grunt.registerTask('default', ['nodeunit:all', 'clean:dist', 'copy:main']);
+    grunt.registerTask('doc', ['yuidoc']);
+};
