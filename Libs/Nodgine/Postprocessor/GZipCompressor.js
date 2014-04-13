8a6b9f8821578a97045b454f886d3998044cdd61
Steffen <sateffen@gmail.com>
2014-04-13 14:48:49 +0200

    Added an gzip postprocessor

---
+++ Libs/Nodgine/Postprocessor/GZipCompressor.js
@@ -0,0 +1,25 @@
+
+var mZlib = require('zlib');
+
+module.exports = function(aRequest, aResponse, aArgs, aCallback) {
+    'use strict';
+
+    if (aRequest.headers['accept-encoding'] && aRequest.headers['accept-encoding'].indexOf('gzip') > -1) {
+        var tmp = aResponse.nodgineGetWroteData();
+        aResponse.nodgineResetWroteData();
+        mZlib.gzip(tmp, function(error, result) {
+            if (error) {
+                aResponse.write(tmp);
+            }
+            else {
+                aResponse.setHeader('Content-Encoding', 'gzip');
+                aResponse.write(result);
+            }
+
+            aCallback();
+        });
+    }
+    else {
+        aCallback();
+    }
+};
