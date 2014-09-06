7404eb8a00441581e70201c9af99f034c00a987c
Steffen <sateffen@gmail.com>
2014-09-06 09:52:58 +0200

    Fix to preprocessor for postdata

--- Lib/Nodgine/Preprocessor/Parser/Post.js
+++ Lib/Nodgine/Preprocessor/Parser/Post.js
@@ -6,7 +6,7 @@ module.exports = function(aRequest, aResponse, aArgs, aCallback) {
     if (aRequest.headers['content-type'] && aRequest.headers['content-type'].toLowerCase().indexOf('application/json') > -1) {
         // try to parse it, baby
         try {
-            aRequest.post = JSON.parse(aRequest.post);
+            aRequest.post = JSON.parse(aRequest.post.toString());
         }
             // nope, the parser didn't like it
         catch(e) {
