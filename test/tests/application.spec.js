af8c761429ccdbd21b85e181c6cc394a4bca7778
Steffen <sateffen@gmail.com>
2015-01-07 15:33:55 +0100

    Added some small test cases

--- test/tests/application.spec.js
+++ test/tests/application.spec.js
@@ -1,5 +1,34 @@
 'use strict';
 
-describe('$APPLICATION should work as intended', function () {
+var chai = require('chai'),
+    expect = chai.expect,
+    mPath = require('path'),
+    mApplication = require('../../src/bootstrap.js').$APPLICATION;
 
+describe('$APPLICATION should work as intended', function () {
+    it('should emit the run event on runApplication', function (done) {
+        mApplication.once('startApplication', function () {
+            expect(true).to.equal(true);
+            done();
+        });
+
+        function runApplication() {
+            mApplication.runApplication();
+        }
+
+        expect(runApplication).not.to.throw();
+    });
+
+    it('should emit the stop event on stopApplication', function (done) {
+        mApplication.once('stopApplication', function () {
+            expect(true).to.equal(true);
+            done();
+        });
+
+        function stopApplication() {
+            mApplication.stopApplication();
+        }
+
+        expect(stopApplication).not.to.throw();
+    });
 });
