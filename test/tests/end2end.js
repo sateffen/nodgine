bcdb53c57ba086dbd02192e15938004242721966
Steffen W <sateffen@gmail.com>
2015-12-26 20:20:13 +0100

    Fixed an eslint error

--- test/tests/end2end.js
+++ test/tests/end2end.js
@@ -43,7 +43,7 @@ describe('End to end functionallity', () => {
                 aResponse.write('async');
                 process.nextTick(() => {
                     aResolve();
-                })
+                });
             });
         });
 
