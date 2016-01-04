1cb8ba1cd77122eeb821a9f2d7cc53e0131bcbe0
Steffen W <sateffen@gmail.com>
2016-01-04 16:49:36 +0100

    Added some examples

---
+++ docs/examples/helloworld.js
@@ -0,0 +1,29 @@
+// this example simply returns hello world to EVERY request
+'use strict';
+
+// first you have to load the nodgine. Usually you'll call require('nodgine'),
+// but here I'll refer to the module itself, so the example works
+const Nodgine = require('../../');
+// and load the http lib from node. If you want to use https, just load https
+const libHttp = require('http');
+
+// then create an instance of the nodgine. I don't have any good idea for a name,
+// so I'll name it instance
+let instance = new Nodgine();
+// then I create a server, where I'll pass the router of the instance. You can receive
+// it by calling instance.getRouter(). This will return a routing function for the
+// http server.
+let server = libHttp.createServer(instance.getRouter());
+
+// then we add a controller to the instance
+instance.addController('/*', (request, response) => {
+    // that simply writes the statuscode and a text to the response
+    response
+        .setStatusCode(200)
+        .write('Hello World');
+    // you do not need to flush the response, flushing is done automatically
+    // we don't return a promise, so this controller is handled as synchron request
+});
+
+// finally call listen
+server.listen(8888);
---
+++ docs/examples/todoserver.js
@@ -0,0 +1,84 @@
+// in this example we will write a server, that does the same as a todo mvc,
+// but everything works via this server
+'use strict';
+
+// so we start of like in the helloworld example. If you didn't check it
+// and you've got a question about this, please refer to helloworld.js
+const Nodgine = require('../../');
+const libHttp = require('http');
+const libFs = require('fs');
+
+let instance = new Nodgine();
+let server = libHttp.createServer(instance.getRouter());
+const toDoHash = {};
+
+// now we have the same setup like in the helloworld.js example. Now we have to
+// add the actual code
+
+// first we add a middleware, that sets the content header to json, so we don't
+// have to do it every time again. This applies only to api requests
+instance.addMiddleware('/api/*', (request, response) => {
+    response.setHeader('content-type', 'application/json');
+});
+
+// Here we add a servelet, that handles the requests for get post and delete.
+// This is capable of more than the simple html client can, but it's all just
+// an example
+instance.addController('/api/todo/:id?', {
+    doGet: (request, response, params) => {
+        if (params.id === undefined) {
+            response
+                .setStatusCode(200)
+                .write(JSON.stringify(toDoHash));
+        }
+        else {
+            if (toDoHash[params.id]) {
+                response
+                    .setStatusCode(200)
+                    .write(JSON.stringify(toDoHash[params.id]));
+            }
+            else {
+                response.setStatusCode(404);
+            }
+        }
+    },
+    doPost: (request, response, params) => {
+        if (params.id === undefined) {
+            response.setStatusCode(404);
+        }
+        else {
+            toDoHash[params.id] = request.getBody().toString();
+            response
+                .setStatusCode(200)
+                .write(JSON.stringify(toDoHash[params.id]));
+        }
+    },
+    doDelete: (request, response, params) => {
+        if (params.id !== undefined) {
+            toDoHash[params.id] = undefined;
+        }
+
+        response.setStatusCode(200);
+    }
+});
+
+// and we add another controller, that simply returns the html file. The file is not
+// cached, it is read every time, so there might be a short lock, but this is just an
+// example for an async controller. Imagine a database here.
+instance.addController('/', (request, response) => {
+    return new Promise((resolve, reject) => {
+        libFs.readFile(__dirname + '/todoserver.html', (err, content) => {
+            if (err) throw err;
+            
+            response
+                .setStatusCode(200)
+                .setHeader('content-type', 'text/html')
+                .write(content);
+
+            resolve();
+        });
+    });
+});
+
+// then start the actual server
+server.listen(8888);
